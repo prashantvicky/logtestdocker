ec709b711b068b4ad1ad76fd7b674e8ccd23cf98
prashantvicky <prashant.vicky@gmail.com>
2018-05-14 18:31:47 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+while [ 1 ]
+do
+   instruction=$(shuf -i 0-4 -n 1)
+   d=`date -Iseconds`
+   case "$instruction" in
+      "1") echo "$d ERROR something happened in this execution."
+      ;;
+      "2") echo "$d INFO takes the value and converts it to string."
+      ;;
+      "3") echo "$d WARN variable not in use."
+      ;;
+      "4") echo "$d DEBUG first loop completed."
+      ;;
+   esac
+done
