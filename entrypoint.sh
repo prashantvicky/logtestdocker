79cd7c320263635efd098d7b26a6fb2c65c7d68f
prashantvicky <prashant.vicky@gmail.com>
2018-06-15 11:16:52 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ do
    instruction=$(shuf -i 0-4 -n 1)
    d=`date -Iseconds`
    case "$instruction" in
-      "1") echo "$d ERROR something happened in this execution."
+      "1") echo "2017-Sep-05+09:15:00.456 [aaamgr 21112 trace] [2/0/2222 <sessmgr:1> hsgw_funcs.c:222]      [software internal user syslog] HSGW Driver: Driver FSM state transition from              HSGWDRV_STATE_OPEN to HSGWDRV_STATE_NEWCALL_ARRIVED"
       ;;
-      "2") echo "$d INFO takes the value and converts it to string."
+      "2") echo "2017-Sep-05+09:15:00.456 [aaamgr 21112 trace] [2/0/2222 <sessmgr:1> hsgw_funcs.c:222]      [software internal user syslog] HSGW Driver: Driver FSM state transition from              HSGWDRV_STATE_OPEN to HSGWDRV_STATE_NEWCALL_ARRIVED"
       ;;
-      "3") echo "$d WARN variable not in use."
+      "3") echo "2017-Sep-05+09:15:00.456 [aaamgr 21112 trace] [2/0/2222 <sessmgr:1> hsgw_funcs.c:222]      [software internal user syslog] HSGW Driver: Driver FSM state transition from              HSGWDRV_STATE_OPEN to HSGWDRV_STATE_NEWCALL_ARRIVED"
       ;;
-      "4") echo "$d DEBUG first loop completed."
+      "4") echo "2017-Sep-05+09:15:00.456 [aaamgr 21112 trace] [2/0/2222 <sessmgr:1> hsgw_funcs.c:222]      [software internal user syslog] HSGW Driver: Driver FSM state transition from              HSGWDRV_STATE_OPEN to HSGWDRV_STATE_NEWCALL_ARRIVED"
       ;;
    esac
 done
