fb65562d0fe98714ec78eae8691cec310e7d7135
prashantvicky <prashant.vicky@gmail.com>
2018-06-19 18:16:03 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ do
    instruction=$(shuf -i 0-4 -n 1)
    d=`date -Iseconds`
    case "$instruction" in
-      "1") echo "2017-Sep-05+09:15:00.455 [aaamgr 21113 trace]" > log.log
+      "1") echo "2017-Sep-05+09:15:00.455 [aaamgr 21113 trace]" >> var/log/log.log
       ;;
-      "2") echo "2017-Sep-05+09:15:00.456 [aaamgr 21114 trace]" > log.log
+      "2") echo "2017-Sep-05+09:15:00.456 [aaamgr 21114 trace]" >> var/log/log.log
       ;;
-      "3") echo "2017-Sep-05+09:15:00.457 [aaamgr 21115 trace]" > log.log
+      "3") echo "2017-Sep-05+09:15:00.457 [aaamgr 21115 trace]" >> var/log/log.log
       ;;
-      "4") echo "2017-Sep-05+09:15:00.458 [aaamgr 21112 trace]" > log.log
+      "4") echo "2017-Sep-05+09:15:00.458 [aaamgr 21112 trace]" >> var/log/log.log
       ;;
    esac
 done
